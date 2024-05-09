71067c89cc8e59e02105b281c45a6447c1e3c2ac
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-05-09 19:31:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-disableSmoothing();
-
 console.log(canvas.width);
 
-var x = 50;
-var y = 50;
-var pixelWidth = 1;
-var pixelHeight = 1;
+var x = 0;
+var y = 0;
+var pixelWidth = 10;
+var pixelHeight = 10;
 ctx.fillStyle = 'black';
 ctx.fillRect(x, y, pixelWidth, pixelHeight);
 
-disableSmoothing();
 
 
-function disableSmoothing()
-{
-  ctx.imageSmoothingEnabled       = false;
-  ctx.webkitImageSmoothingEnabled = false;
-  ctx.mozImageSmoothingEnabled    = false;
-  ctx.msImageSmoothingEnabled     = false;
-  ctx.oImageSmoothingEnabled      = false;  
-}
+
