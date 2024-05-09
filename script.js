931b17835bf5622faeb711ac2690eadd4b9e86d3
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-05-10 00:56:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,149 +13,120 @@ var changedTiles = [];
 console.log(canvas.width);
 console.log(canvas.height);
 
-class Tile
-{
-  constructor(x, y, isAlive)
-  {
-    this.x = x;
-    this.y = y;
-    this.isAlive = isAlive;
-    this.neighbors = [];
-    this.isChecked = false;
-  }
-}
-
-for (let i = 0; i < columns; i++) 
-{
-    my2DArray[i] = [];
-    for (let j = 0; j < rows; j++) 
-    {
-      my2DArray[i][j] = new Tile(i, j, Math.random() < 0.5);
-      tiles.push(my2DArray[i][j]);
-      if(my2DArray[i][j].isAlive)
-      {
-        drawPixel(i, j);
-        aliveTiles.push(my2DArray[i][j]);
-      }
+class Tile {
+    constructor(x, y, isAlive) {
+        this.x = x;
+        this.y = y;
+        this.isAlive = isAlive;
+        this.neighbors = [];
+        this.isChecked = false;
     }
 }
-for (let i = 0; i < columns; i++) 
-  {
-      for (let j = 0; j < rows; j++) 
-      {
-        if(i - 1 >= 0)
-        {
-          //X00
-          //X00
-          //X00
-          my2DArray[i][j].neighbors.push(my2DArray[i - 1][j]);
-          
-          if(j - 1 >= 0)
-          {
-            my2DArray[i][j].neighbors.push(my2DArray[i - 1][j - 1]);
-          }
-            
-          if(j + 1 < rows)
-          {
-            my2DArray[i][j].neighbors.push(my2DArray[i - 1][j + 1]);
-          }
-            
-        }
-        if(i + 1 < columns)
-        {
-            //00X
-            //00X
-            //00X
-            my2DArray[i][j].neighbors.push(my2DArray[i + 1][j]);
-            
-            if(j - 1 >= 0)
-            {
-              my2DArray[i][j].neighbors.push(my2DArray[i + 1][j - 1]);
-            }
-              
-            if(j + 1 < rows)
-            {
-              my2DArray[i][j].neighbors.push(my2DArray[i + 1][j + 1]);
-            }
-              
-        }
-        if(j + 1 < rows){
-          my2DArray[i][j].neighbors.push(my2DArray[i][j + 1]);
-        }
-        if(j - 1 >= 0){
-          my2DArray[i][j].neighbors.push(my2DArray[i][j - 1]);
-        } 
 
-      }
-
-      
-  }
-
-
+setup();
 setInterval(gameLoop, 75);
 
-
-function gameLoop()
-{
-  for(const tile of aliveTiles){
-    checkTile(tile);
-  }
-
-  for(const tile of changedTiles){
-    if(!tile.isAlive){
-      aliveTiles.push(tile);
-      tile.isAlive = true;
-      drawPixel(tile.x, tile.y, 'black');
+function setup() {
+    for (let i = 0; i < columns; i++) {
+        my2DArray[i] = [];
+        for (let j = 0; j < rows; j++) {
+            my2DArray[i][j] = new Tile(i, j, Math.random() < 0.5);
+            tiles.push(my2DArray[i][j]);
+            if (my2DArray[i][j].isAlive) {
+                drawPixel(i, j);
+                aliveTiles.push(my2DArray[i][j]);
+            }
+        }
     }
-    else{
-      aliveTiles.splice(aliveTiles.indexOf(tile), 1);
-      tile.isAlive = false;
-      drawPixel(tile.x, tile.y, 'white');
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (i - 1 >= 0) {
+                my2DArray[i][j].neighbors.push(my2DArray[i - 1][j]);
+
+                if (j - 1 >= 0) {
+                    my2DArray[i][j].neighbors.push(my2DArray[i - 1][j - 1]);
+                }
+
+                if (j + 1 < rows) {
+                    my2DArray[i][j].neighbors.push(my2DArray[i - 1][j + 1]);
+                }
+
+            }
+            if (i + 1 < columns) {
+                my2DArray[i][j].neighbors.push(my2DArray[i + 1][j]);
+
+                if (j - 1 >= 0) {
+                    my2DArray[i][j].neighbors.push(my2DArray[i + 1][j - 1]);
+                }
+
+                if (j + 1 < rows) {
+                    my2DArray[i][j].neighbors.push(my2DArray[i + 1][j + 1]);
+                }
+
+            }
+            if (j + 1 < rows) {
+                my2DArray[i][j].neighbors.push(my2DArray[i][j + 1]);
+            }
+            if (j - 1 >= 0) {
+                my2DArray[i][j].neighbors.push(my2DArray[i][j - 1]);
+            }
+
+        }
+
 
     }
-  }
-  for(const tile of tiles){
-    tile.isChecked = false;
-  }
-
-  changedTiles = [];
 }
 
-function checkTile(tile)
-{
-  var neighborCount = 0;
+function gameLoop() {
+    for (const tile of aliveTiles) {
+        checkTile(tile);
+    }
 
-  for(const neighbor of tile.neighbors)
-  {
-    if(neighbor.isAlive){
-      neighborCount++;
-    }
-    else if(!neighbor.isChecked && tile.isAlive){
-      neighbor.isChecked = true;
-      checkTile(neighbor);
-    }
-  }
+    for (const tile of changedTiles) {
+        if (!tile.isAlive) {
+            aliveTiles.push(tile);
+            tile.isAlive = true;
+            drawPixel(tile.x, tile.y, 'black');
+        } else {
+            aliveTiles.splice(aliveTiles.indexOf(tile), 1);
+            tile.isAlive = false;
+            drawPixel(tile.x, tile.y, 'white');
 
-  if(tile.isAlive){
-    if(neighborCount < 2 ){
-      changedTiles.push(tile);
+        }
     }
-    else if(neighborCount > 3){
-      changedTiles.push(tile);
+    for (const tile of tiles) {
+        tile.isChecked = false;
     }
-  }
-  else{
-    if(neighborCount == 3){
-      changedTiles.push(tile);
-    }
-  }
+
+    changedTiles = [];
 }
 
-function drawPixel(x, y, color)
-{
-  ctx.fillStyle = color;
-  ctx.fillRect(x*10, y*10, 10, 10);
+function checkTile(tile) {
+    var neighborCount = 0;
+
+    for (const neighbor of tile.neighbors) {
+        if (neighbor.isAlive) {
+            neighborCount++;
+        } else if (!neighbor.isChecked && tile.isAlive) {
+            neighbor.isChecked = true;
+            checkTile(neighbor);
+        }
+    }
+
+    if (tile.isAlive) {
+        if (neighborCount < 2) {
+            changedTiles.push(tile);
+        } else if (neighborCount > 3) {
+            changedTiles.push(tile);
+        }
+    } else {
+        if (neighborCount == 3) {
+            changedTiles.push(tile);
+        }
+    }
 }
 
-
-
+function drawPixel(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * 10, y * 10, 10, 10);
+}
