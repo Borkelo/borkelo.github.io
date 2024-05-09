eda0cc290e9cdfb6c18b0d65c33e4ab9bc5ac088
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-05-09 20:21:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var ctx = canvas.getContext('2d');
 
 console.log(canvas.width);
 
-var x = 0;
-var y = 0;
-var pixelWidth = 10;
-var pixelHeight = 10;
-ctx.fillStyle = 'black';
-ctx.fillRect(x, y, pixelWidth, pixelHeight);
+drawPixel(0,0);
+drawPixel(0,1);
+
+function drawPixel(x, y)
+{
+  ctx.fillStyle = 'black';
+  ctx.fillRect(x*10, y*10, 10, 10);
+}
 
 
 
