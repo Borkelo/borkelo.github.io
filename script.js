4879f9c758540dfecc9d5d9d0665115e8dc87ade
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-05-09 18:25:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
 
+var x = 50;
+var y = 50;
+var pixelWidth = 15;
+var pixelHeight = 15;
+ctx.fillStyle = 'black';
+ctx.fillRect(x, y, pixelWidth, pixelHeight);
