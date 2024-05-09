c9bf7aef7fd098dbd3ecf57df09de476f465c477
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-05-09 20:38:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-console.log(canvas.width);
 
 drawPixel(0,0);
 drawPixel(1,1);
+
+const my2DArray = [];
+const columns = canvas.width;
+const rows = canvas.height;
+
+console.log(canvas.height);
+console.log(canvas.height);
+
+for (let i = 0; i < rows; i++) {
+  my2DArray[i] = [];
+  for (let j = 0; j < columns; j++) {
+    my2DArray[i][j] = j;
+  }
+}
+
+
+
+function setup()
+{
+
+}
+
+function gameLoop()
+{
+
+}
 
 function drawPixel(x, y)
 {
@@ -12,6 +37,14 @@ function drawPixel(x, y)
   ctx.fillRect(x*10, y*10, 10, 10);
 }
 
-
+class Tile
+{
+  constructor(x, y, isAlive)
+  {
+    this.x = x;
+    this.y = y;
+    this.isAlive = isAlive;
+  }
+}
 
 
