11046c308dc69964eac67d4fde8a3d82afda9f1f
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-11-29 21:08:08 +0200

    Update script.js

--- song/script.js
+++ song/script.js
@@ -4,7 +4,7 @@ window.onload = () => {
 
     // Define the SoundCloud track URL dynamically
     const trackUrl = 'https://soundcloud.com/rick-astley-official/never-gonna-give-you-up-4'; // Replace with your desired track URL
-    const embedUrl = `https://w.soundcloud.com/player/?url=${encodeURIComponent(trackUrl)}&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=false`;
+    const embedUrl = `https://w.soundcloud.com/player/?url=${encodeURIComponent(trackUrl)}&hide_related=true&show_comments=false&show_user=false&show_reposts=false&visual=false`;
 
     // Set the iframe src to load the desired track
     iframe.src = embedUrl;
