a900eca6136b55082466d854c8f0fa8a7e59c991
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-11-30 01:37:24 +0200

    added song guess

--- guess-the-song/script.js
+++ guess-the-song/script.js
@@ -1,9 +1,86 @@
 const audioPlayer = document.getElementById('audioplayer');
 const playButton = document.getElementById('playButton');
+const guessButton = document.getElementById('guessButton');
+const inputText = document.getElementById('input');
+const correctAnswerText = document.getElementById('correctAnswerText');
+const durationDisplay = document.getElementById('durationDisplay');
 
-function playAudio(filePath) {
-  audioPlayer.src = 'songs/0Never_Gonna_Give_You_Up.mp3';
-  audioPlayer.play();
+const durations = [0.1, 0.5, 2, 4, 8, 15]
+let currentIndex = 0;
+
+let randomStart;
+let answer;
+let fuse;
+let gameover = false;
+
+function setup(){
+    audioPlayer.src = 'songs/Never_Gonna_Give_You_Up.mp3';
+    
+    answer = "never gonna give you up";
+    fuse = new Fuse([answer], {
+        includeScore: true,
+        threshold: 0.5,
+      });
+
+    durationDisplay.textContent = durations[currentIndex] + " s";
+    
+    audioPlayer.onloadedmetadata = function() {
+        let duration = audioPlayer.duration;
+        randomStart = Math.random() * (duration - 15);
+    };   
 }
 
+function endGame(){
+    gameover = true;
+    audioPlayer.currentTime = 0;
+    audioPlayer.play();
+    correctAnswerText.textContent = answer;
+}
+
+function playAudio() {
+    if(!audioPlayer.paused || gameover){
+        return;
+    }
+    
+    audioPlayer.currentTime = randomStart;   
+    audioPlayer.play();
+
+    setTimeout(() => {
+        audioPlayer.pause();
+    }, durations[currentIndex] * 1000);
+}
+
+function checkText(text) {
+    text = text.trim().toLowerCase();
+    let result = fuse.search(text);
+  
+    if (result.length > 0 && result[0].score < 0.5) {
+      return true;
+    }
+    return false;    
+  }
+
+function guessSong(){
+    if(gameover){
+        return;
+    }
+
+    if(checkText(inputText.value)){
+        endGame();
+        return;
+    }
+
+    if(currentIndex === 5){
+        endGame();
+        return;
+    }
+
+    currentIndex += 1;
+    durationDisplay.textContent = durations[currentIndex] + " s";
+}
+
+
+
+setup();
 playButton.addEventListener('click', playAudio);
+guessButton.addEventListener('click', guessSong)
