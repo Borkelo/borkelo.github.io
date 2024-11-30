f8b4050b97004cf7a1e2e934d7a0c9b849312b8f
Borkelo <103523885+Borkelo@users.noreply.github.com>
2024-11-30 04:49:25 +0200

    Update script.js

--- guess-the-song/script.js
+++ guess-the-song/script.js
@@ -56,6 +56,9 @@ function endGame(){
 }
 
 async function playAudio() {
+    if (!widget) {
+        return;
+    }
     
     let isPaused = await new Promise(resolve => {
         widget.isPaused(function(paused) {
