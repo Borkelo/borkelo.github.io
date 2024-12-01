6dec5b0032d6b0c46293aac111fee8018b9da047
ThePhantomViper <renelucas.nordin@gmail.com>
2024-12-01 21:50:43 +0200

    Update script.js

--- guess-the-song-kendrick/script.js
+++ guess-the-song-kendrick/script.js
@@ -6,7 +6,7 @@ const correctAnswerText = document.getElementById('correctAnswerText');
 const durationDisplay = document.getElementById('durationDisplay');
 
 var widget;
-const durations = [0.5, 2, 4, 8, 15, 25]
+const durations = [0.2, 0.5, 2, 4, 8, 15, 25]
 const guessTreshold = 0.3;
 
 let currentIndex = 0;
